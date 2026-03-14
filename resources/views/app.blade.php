caddf3b105c94c0ff9fa9ae6d5c285e33e965937
Timothy Justin Oesch <timothy@kpunkt.ch>
2026-03-14 21:27:48 +0100

    Add favicon and webmanifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,12 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/img/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/img/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/img/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Pollendar" />
+        <link rel="manifest" href="/site.webmanifest" />
         @vite('resources/frontend/js/app.js')
         @inertiaHead
     </head>
